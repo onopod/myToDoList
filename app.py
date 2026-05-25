06fb016522232808bf17ac9bdbe4e2c077eb51dc
onopod <onopod@gmail.com>
2026-05-25 13:18:15 +0900

    addを追加2

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "こんにちは"
+
+
+@app.route("/add")
+def add():
+    if # POSTで飛んで来たら:
+        # フォームの内容チェック
+        if # 正しければ:
+            # insert
+            # 詳細画面にリダイレクト
+        
